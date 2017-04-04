c10e60b2516eccb69c397de6ecd3ace7fbf2b08d
HelloWood <hellowoodes@gmail.com>
2017-04-04 15:32:06 +0800

    Add config property file

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 //app.js
+var properties = require("/utils/configProperties.js")
 App({
-  onLaunch: function () {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-  },
-  getUserInfo:function(cb){
-    var that = this;
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo;
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
+    onLaunch: function() {
+        //调用API从本地缓存中获取数据
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
+        console.log(wx.getSystemInfoSync())
+    },
+    getUserInfo: function(cb) {
+        var that = this;
+        if (this.globalData.userInfo) {
+            typeof cb == "function" && cb(this.globalData.userInfo)
+        } else {
+            //调用登录接口
+            wx.login({
+                success: function() {
+                    wx.getUserInfo({
+                        success: function(res) {
+                            that.globalData.userInfo = res.userInfo;
+                            typeof cb == "function" && cb(that.globalData.userInfo)
+                        }
+                    })
+                }
+            });
         }
-      });
-    }
-  },
-  globalData:{
-    userInfo:null
-  }
+    },
+    globalData: {
+        userInfo: null,
+        userId: '',
+        authorization: properties.authorization,
+        academicUrl: properties.academicUrl,
+        loginUrl:properties.loginUrl,
+        studentInfoUrl: properties.studentInfoUrl,
+        examUrl: properties.examUrl,
+        scheduleUrl: properties.scheduleUrl,
+        skillInfoUrl: properties.skillInfoUrl,
+        toastFailImg: properties.toastFailImg
+    },
 })
-
-
-
---
+++ utils/configProperties.js
@@ -0,0 +1,19 @@
+  var authorization = "";
+  var academicUrl = "";
+  var loginUrl = "";
+  var studentInfoUrl = "";
+  var examUrl = "";
+  var scheduleUrl = "";
+  var skillInfoUrl = "";
+  var toastFailImg = "../images/fail.png"
+  
+  module.exports = {
+      authorization: authorization,
+      academicUrl: academicUrl,
+      loginUrl: loginUrl,
+      studentInfoUrl: studentInfoUrl,
+      examUrl: examUrl,
+      scheduleUrl: scheduleUrl,
+      skillInfoUrl: skillInfoUrl,
+      toastFailImg: toastFailImg
+  }
