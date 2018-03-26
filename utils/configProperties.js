e110e852a50b30ab1d177d04de7d20bc381df7fc
何惠民 <huimin.he@yi23.net>
2018-03-26 18:32:36 +0800

    Changed path of education plan

--- utils/configProperties.js
+++ utils/configProperties.js
@@ -1,10 +1,31 @@
-var appid = "";
-var secret = "";
-var wxGlobalToken = "";
-var authorization = "";
-var ServerURL = "";
+// var appid = "";
+// var secret = "";
+// var wxGlobalToken = "";
+// var authorization = "";
+// var ServerURL = "";
+// var toastFailImg = "/images/global/fail.png";
+// var version = "0.0.3";
+// var versionType = "开发版";
+//
+// module.exports = {
+//     appid: appid,
+//     secret: secret,
+//     wxGlobalToken: wxGlobalToken,
+//     authorization: authorization,
+//     ServerURL: ServerURL,
+//     toastFailImg: toastFailImg,
+//     version: version,
+//     versionType: versionType
+// }
+
+var appid = "wx792078d238b3eec8";
+var secret = "1272b93b03a16624394463f6bc7acc37";
+var wxGlobalToken = "4878ED34CBFADC5D8E4849E667B1AC62";
+var authorization = "jfklsadj342380#@$#@DSfsdgdfsg";
+var ServerURL = "http://localhost:8080/oe/api";
+// var ServerURL = "http://oe.hellowood.com.cn:8088/oe/oe/api";
 var toastFailImg = "/images/global/fail.png";
-var version = "0.0.3";
+var version = "0.0.2";
 var versionType = "开发版";
 
 module.exports = {
@@ -17,3 +38,4 @@ module.exports = {
     version: version,
     versionType: versionType
 }
+
