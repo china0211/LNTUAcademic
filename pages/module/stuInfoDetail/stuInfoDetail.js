b84f8708497c66b03c9ac78f184a4c8c6d9ba77d
何惠民 <huimin.he@yi23.net>
2018-03-27 00:13:40 +0800

    Add loading and no data to student info

--- pages/module/stuInfoDetail/stuInfoDetail.js
+++ pages/module/stuInfoDetail/stuInfoDetail.js
@@ -1,23 +1,37 @@
 var app = getApp();
+var util = require("../../../utils/util.js");
 Page({
     data: {
-        stuDetail: null
+        stuDetail: null,
+        loading: true,
+        noData: false
     },
     onLoad: function (options) {
         app.mta.Page.init();
-        // app.validateStuId();
         var that = this;
         wx.getStorage({
             key: 'stuDetail',
             success: function (res) {
-                that.setData({
-                    stuDetail: res.data
-                })
+                if (util.isEmpty(res.data)) {
+                    that.setData({
+                        noData: true
+                    })
+                } else {
+                    that.setData({
+                        noData: false,
+                        stuDetail: res.data
+                    })
+                }
             },
             fail: function (res) {
                 app.showToast("读取用户信息失败，请重新登录", false);
                 app.redirectToLoginPage();
+            },
+            complete: function (res) {
+                that.setData({
+                    loading: false
+                });
             }
-        })
+        });
     }
 })
