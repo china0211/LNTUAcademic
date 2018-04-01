a662f52a7f3d3d0e3cfc6e4adbf6d135cc9736b6
HelloWood <hellowoodes@gmail.com>
2018-04-01 22:35:45 +0800

    Fixed issue of config

--- utils/configProperties.js
+++ utils/configProperties.js
@@ -1,8 +1,30 @@
 var authorization = "";
 var appID = "";
 var eventID = "";
-var authorization = "";
-var ServerURL = "";
+
+var SERVER_URL = "";
+var getOpenIdUrl = SERVER_URL + "";
+var loginUrl = SERVER_URL + "";
+var studentInfoUrl = SERVER_URL + "";
+var examPlanUrl = SERVER_URL + "";
+var courseScheduleUrl = SERVER_URL + "";
+var gradeExamScoreUrl = SERVER_URL + "";
+var examUrl = SERVER_URL + "";
+var gradePointUrl = SERVER_URL + "";
+var bindUrl = SERVER_URL + "";
+var unBindUrl = SERVER_URL + "";
+var feedbackUrl = SERVER_URL + "";
+var feedbackUploadUrl = SERVER_URL + "";
+var announcementUrl = SERVER_URL + "";
+var modifyPasswordUrl = SERVER_URL + "";
+var classroomOccupyUrl = SERVER_URL + "";
+var getTokenUrl = SERVER_URL + "";
+var courseStudyScheduleUrl = SERVER_URL + "";
+var getStudentNoUrl = SERVER_URL + "";
+var saveStartUpRecordUrl = SERVER_URL + "";
+var getCurrentTimeInfoUrl = SERVER_URL + "";
+var evaluateCourseUrl = SERVER_URL + "";
+
 var toastFailImg = "/images/global/fail.png";
 var version = "0.0.3";
 var versionType = "开发版";
@@ -11,8 +33,29 @@ module.exports = {
     authorization: authorization,
     appID: appID,
     eventID: eventID,
-    ServerURL: ServerURL,
+    SERVER_URL: SERVER_URL,
     toastFailImg: toastFailImg,
     version: version,
-    versionType: versionType
+    versionType: versionType,
+    getOpenIdUrl: getOpenIdUrl,
+    loginUrl: loginUrl,
+    studentInfoUrl: studentInfoUrl,
+    examPlanUrl: examPlanUrl,
+    courseScheduleUrl: courseScheduleUrl,
+    gradeExamScoreUrl: gradeExamScoreUrl,
+    examUrl: examUrl,
+    gradePointUrl: gradePointUrl,
+    bindUrl: bindUrl,
+    unBindUrl: unBindUrl,
+    feedbackUrl: feedbackUrl,
+    feedbackUploadUrl: feedbackUploadUrl,
+    announcementUrl: announcementUrl,
+    modifyPasswordUrl: modifyPasswordUrl,
+    classroomOccupyUrl: classroomOccupyUrl,
+    getTokenUrl: getTokenUrl,
+    courseStudyScheduleUrl: courseStudyScheduleUrl,
+    getStudentNoUrl: getStudentNoUrl,
+    saveStartUpRecordUrl: saveStartUpRecordUrl,
+    getCurrentTimeInfoUrl: getCurrentTimeInfoUrl,
+    evaluateCourseUrl: evaluateCourseUrl
 };
