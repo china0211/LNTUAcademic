d7bb3e3ee2ec351717cf63a65c3354f158144b4a
helloworlde <hellowoodes@gmail.com>
2018-05-17 21:54:01 +0800

    Add authorization button on me page

--- pages/more/me/me.js
+++ pages/more/me/me.js
@@ -1,12 +1,19 @@
 var app = getApp();
+var util = require("../../../utils/util.js");
 Page({
     data: {
         studentDetail: null,
         userInfo: null,
-        isBind: false
+        isBind: false,
+        authorization: true
     },
     onLoad: function (options) {
         var that = this;
+        if (util.isEmpty(app.globalData.userInfo)) {
+            that.setData({
+                authorization: false
+            });
+        }
         that.setData({
             studentDetail: app.globalData.studentDetail,
             userInfo: app.globalData.userInfo,
@@ -15,6 +22,21 @@ Page({
         app.mta.Page.init();
     },
     onShow: function () {
-
+    },
+    getUserInfo: function () {
+        var that = this;
+        if (util.isEmpty(that.data.userInfo)) {
+            wx.getUserInfo({
+                success: function (resp) {
+                    if (!util.isEmpty(resp.userInfo)) {
+                        that.setData({
+                            userInfo: resp.userInfo,
+                            authorization: true
+                        });
+                        app.globalData.userInfo = resp.userInfo;
+                    }
+                }
+            });
+        }
     }
 });
